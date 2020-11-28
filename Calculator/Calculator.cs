3bc1913f21f8f78e641422dc66adfa663353d3b3
qusterrr <goshan.ved1997@gmail.com>
2020-11-28 15:36:23 +0300

    memoryupdate
    added memory function

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace LidlCalculator
+{
+    public class Calculator
+    {
+        public void Start()
+        {
+            double memory = 0;
+            Console.WriteLine("Напишите первое число");
+            double result = EnterNumber();
+
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Введите число");
+                double number = EnterNumber();
+
+                Console.WriteLine("Что вы хотите сделать?");
+                Console.WriteLine("\tplus - сложить");
+                Console.WriteLine("\tminus - вычесть");
+                Console.WriteLine("\tmultiply - умножить");
+                Console.WriteLine("\tdivide - разделить");
+                Console.WriteLine("\tplusmem -  сохраненный результат");
+                Console.WriteLine("\tminusmem - вычесть сохраненный результат");
+                Console.WriteLine("\tmultiplymem - умножить на сохраненный результат");
+                Console.WriteLine("\tdividemem - разделить на сохраненный результат");
+
+                Console.Write("Ваш выбор- ");
+
+                switch (Console.ReadLine())
+                {
+                    case "plus":
+                        result = Plus(result, number);
+                        break;
+
+                    case "minus":
+                        result = Minus(result, number);
+                        break;
+
+                    case "multiply":
+                        result = Multiply(result, number);
+                        break;
+
+                    case "divide":
+                        if (number == 0)
+                        {
+                            Console.WriteLine("На ноль делить нельзя!!!");
+                            break;
+                        }
+
+                        result = Divide(result, number);
+                        break;
+
+                    case "plusmem":
+                        result = Plus(number, memory);
+                        break;
+
+                    case "minusmem":
+                        result = Minus(number, memory);
+                        break;
+
+                    case "multiplymem":
+                        result = Multiply(number, memory);
+                        break;
+
+                    case "dividemem":
+                        if (memory == 0)
+                        {
+                            Console.WriteLine("На ноль делить нельзя!!!");
+                            break;
+                        }
+
+                        result = Divide(number, memory);
+                        break;
+                }
+
+                Console.WriteLine("\tmemory - сохранить результат");
+                Console.WriteLine("\tclearmemory - очистить сохраненный результат");
+
+                Console.Write("Ваш выбор- ");
+
+                switch (Console.ReadLine())
+                {
+                    case "memory":
+                        memory = result;
+                        break;
+
+                    case "clearmemory":
+                        memory = 0;
+                        break;
+                }
+
+                Console.WriteLine("Вы хотите продолжить? Y или N");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() == "y");
+        }
+
+        private double EnterNumber()
+        {
+            while (true)
+            {
+                try
+                {
+                    return Convert.ToDouble(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Вы ввели не число!");
+                    Console.ResetColor();
+
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Вы ввели слишком большое по модулю число!");
+                    Console.WriteLine($"Число должно быть больше {double.MinValue} и меньше {double.MaxValue}");
+                    Console.ResetColor();
+
+                    continue;
+                }
+            }
+        }
+
+        private int Plus(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine($"Ваш результат: {num1} + {num2} = " + result);
+            return result;
+        }
+
+        private double Plus(double num1, double num2)
+        {
+            double result = num1 + num2;
+            Console.WriteLine($"Ваш результат: {num1} + {num2} = {result}");
+            return result;
+        }
+
+        private int Minus(int num1, int num2)
+        {
+            int result = num1 - num2;
+            Console.WriteLine($"Ваш результат: {num1} - {num2} = " + result);
+            return result;
+        }
+
+        private double Minus(double num1, double num2)
+        {
+            double result = num1 - num2;
+            Console.WriteLine($"Ваш результат: {num1} - {num2} = " + result);
+            return result;
+        }
+
+        private int Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            Console.WriteLine($"Ваш результат: {num1} * {num2} = " + result);
+            return result;
+        }
+
+        private double Multiply(double num1, double num2)
+        {
+            double result = num1 * num2;
+            Console.WriteLine($"Ваш результат: {num1} * {num2} = " + result);
+            return result;
+        }
+
+        private int Divide(int num1, int num2)
+        {
+            int result = num1 / num2;
+            Console.WriteLine($"Ваш результат: {num1} / {num2} = " + result);
+            return result;
+        }
+
+        private double Divide(double num1, double num2)
+        {
+            double result = num1 / num2;
+            Console.WriteLine($"Ваш результат: {num1} / {num2} = " + result);
+            return result;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,46 +1,11 @@
-﻿using System;
-
-namespace Calculator
+﻿namespace LidlCalculator
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-
-            int num1 = 0; int num2 = 0;
-
-
-            Console.WriteLine("Напишите первое число");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Напишите второе число");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Что вы хотите сделать?");
-            Console.WriteLine("\tplus - сложить");
-            Console.WriteLine("\tminus - вычесть");
-            Console.WriteLine("\tmultiply - умножить");
-            Console.WriteLine("\tdivide - разделить");
-            Console.Write("Ваш выбор- ");
-
-            switch (Console.ReadLine())
-            {
-                case "plus":
-                    Console.WriteLine($"Ваш результат: {num1} + {num2} = " + (num1 + num2));
-                    break;
-                case "minus":
-                    Console.WriteLine($"Ваш результат: {num1} - {num2} = " + (num1 - num2));
-                    break;
-                case "multiply":
-                    Console.WriteLine($"Ваш результат: {num1} * {num2} = " + (num1 * num2));
-                    break;
-                case "divide":
-                    Console.WriteLine($"Ваш результат: {num1} / {num2} = " + (num1 / num2));
-                    break;
-            }
+            Calculator calculator = new Calculator();
+            calculator.Start();
         }
     }
 }
-
-
-
